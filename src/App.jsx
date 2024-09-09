6cc79f7cad18fdffaa32f512ee5941af4f43393d
avca1998 <sfsneed@gmail.com>
2024-09-09 10:02:14 -0700

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header";
-import Footer from './components/Footer'
-// import { useState } from "react";
-import "./App.css";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import Nav from "./components/NavTabs";
 
 function App() {
-
   return (
     <>
       <Header />
-
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
