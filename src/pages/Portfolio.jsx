cb76683e5030540bc0787172b2fd6b4776ca8fed
avca1998 <sfsneed@gmail.com>
2024-09-10 14:21:38 -0700

    portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,7 +2,14 @@ export default function Portfolio() {
   return (
     <div className="mx-5 my-5 fs-5">
       <h1>Portfolio</h1>
-
+      <div className="container">
+        <img className="p-2" src="assets/333.jpeg" alt="picturific" />
+        <img className="p-2" src="assets/333.jpeg" alt="picturific" />
+        <img className="p-2" src="assets/333.jpeg" alt="picturific" />
+        <img className="p-2" src="assets/333.jpeg" alt="picturific" />
+        <img className="p-2" src="assets/333.jpeg" alt="picturific" />
+        <img className="p-2" src="assets/333.jpeg" alt="picturific" />
+      </div>
     </div>
   );
 }
