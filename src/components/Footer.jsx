63f0547d3440262825109fdf625c61d148676963
avca1998 <sfsneed@gmail.com>
2024-09-11 16:33:10 -0700

    react new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
   return (
     <footer className="text-center" >
       {/* <a href="https://github.com/sfsneed70" className="px-2"><img src="assets/github-mark-white.png" alt="github logo" style={iconStyle} /></a> */}
-      <a href="https://github.com/sfsneed70" className="px-2"><img src={images.Github} alt="github logo" style={iconStyle} /></a>
-      <a href="https://www.linkedin.com/in/stephen-f-sneed-40145212/" className="px-2"><img src={images.Linkedin} alt="linkedin logo" style={iconStyle} /></a>
-      <a href="https://stackoverflow.com/users/27269174/stephen-sneed" className="px-2"><img src={images.Stackoverflow} alt="stackoverflow logo" style={iconStyle}/></a>
+      <a href="https://github.com/sfsneed70" className="px-2"><img src={images['Github']} alt="github logo" style={iconStyle} /></a>
+      <a href="https://www.linkedin.com/in/stephen-f-sneed-40145212/" className="px-2"><img src={images['Linkedin']} alt="linkedin logo" style={iconStyle} /></a>
+      <a href="https://stackoverflow.com/users/27269174/stephen-sneed" className="px-2"><img src={images['Stackoverflow']} alt="stackoverflow logo" style={iconStyle}/></a>
    </footer>
   );
 }
