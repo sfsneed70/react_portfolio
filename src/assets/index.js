d7d7d42c96f5ebe343c27f9d7d7950056fc239bf
avca1998 <sfsneed@gmail.com>
2024-09-11 20:44:08 -0700

    portfolio form

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,2 @@
-export { default as Github } from "./github-mark-white.png";
-export { default as Linkedin } from "./In-White-48.png";
-export { default as Stackoverflow } from "./stackoverflow-48.png";
 export { default as Avatar } from "./avatar2.jpg";
 export { default as MyResume } from "./Resume.pdf";
